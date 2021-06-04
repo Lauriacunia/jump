095e361511645e8c43504ce1eaf4e6b7f3f977a7
Lauriacunia <lauriacunia@gmail.com>
2021-06-04 16:39:23 -0300

    Avanzo con el maquetado de CardDetail

--- src/components/CardDetail.js
+++ src/components/CardDetail.js
@@ -7,17 +7,13 @@ import {
     } 
     from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import { findByLabelText } from "@testing-library/dom";
 
-
-const useStyles = makeStyles({
-   title: {
-       color: "white",
-   }
-});
 
 
 const CardDetail = () => {
-    const classes = useStyles();
+   
     let params = useParams();
     let location = useLocation();
 
@@ -25,9 +21,47 @@ const CardDetail = () => {
     console.log(location)
     const [resultado, setResultado] = useState([]);
     
-  const BASE_URL = `https://api.themoviedb.org/3`
-  const ruta = location.pathname
-  const APIKEY = `c30046e601e1f588297bc67b7f52c812`;
+    const BASE_URL = `https://api.themoviedb.org/3`
+    const ruta = location.pathname
+    const APIKEY = `c30046e601e1f588297bc67b7f52c812`;
+
+    const useStyles = makeStyles({
+        cardContainer: {
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "600px",
+            position: "relative",
+            backgroundSize: "cover",
+            backgroundPosition: "center-top",
+            backgroundImage: `url(https://image.tmdb.org/t/p/w370_and_h556_bestv2/${resultado.poster_path})`
+        },
+        dataContainer: {
+            display: "flex",
+            backgroundColor: "transparent",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "80vw",
+            margin: "100px",
+            zIndex: 5,
+        },
+        overlay :{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            height: "100%",
+            width: "100%",
+            backgroundColor: "black",
+            opacity: 0.4,
+        },
+        textContainer: {
+            marginLeft: 100,
+        },
+        title: {
+            color: "white",
+        }
+     });
+     const classes = useStyles();
 
     useEffect(() => {
       
@@ -39,14 +73,41 @@ const CardDetail = () => {
             .then(data => {
                 console.log(data)
                 setResultado(data)
+                
             })
     }, []);
 
     return(
-        <>
-        <h1  className={classes.title}>SOY UNA TARJETA CON DETALLE</h1>
-        <h1 className={classes.title}>{resultado.title || resultado.name}</h1>
-        </>
+        <div>
+            <div className={classes.cardContainer}>
+               <div className={classes.overlay}></div>
+               <div className={classes.dataContainer}>
+                    <img src= {`https://image.tmdb.org/t/p/w370_and_h556_bestv2/${resultado.poster_path}`}></img>
+                    <div className={classes.textContainer}>
+                        <Typography className={classes.title} gutterBottom variant="h3" color="textSecondary">
+                            { resultado.title || resultado.name }
+                        </Typography>
+                        <Typography className={classes.title} gutterBottom variant="subtitle" color="textSecondary">
+                            { resultado.original_title|| resultado.name }
+                        </Typography>
+                        <Typography className={classes.title} gutterBottom variant="body1" color="textSecondary">
+                            {resultado.first_air_date && `Año: ${resultado.first_air_date}`}
+                        </Typography>
+                        <Typography className={classes.title} gutterBottom variant="body1" color="textSecondary">
+                            { resultado.runtime && `Duración: ${resultado.runtime} min` }
+                        </Typography>
+                        <Typography className={classes.title} gutterBottom variant="body1" color="textSecondary">
+                           { resultado.genres && `Genero: ${resultado.genres[1]}` }     
+                        </Typography>
+                        <Typography className={classes.title} gutterBottom variant="body1" color="textSecondary">
+                            { resultado.overview }
+                        </Typography>
+                    </div>
+                </div>
+                
+            </div>
+        
+        </div>
     )
 }
 
