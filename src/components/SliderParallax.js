fa482fd4cb9e25913ac5dcdabd0158fd3f5ffbbf
Lauriacunia <lauriacunia@gmail.com>
2021-06-04 13:16:33 -0300

    Corrijo el componente SliderParallaz para poder avanzar con las tarjetas detalle

--- src/components/SliderParallax.js
+++ src/components/SliderParallax.js
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper.min.css";
 import "swiper/components/pagination/pagination.min.css"
 import "swiper/components/navigation/navigation.min.css"
+import "./SliderParallax.css";
 
 import SwiperCore, {
   Parallax, Navigation, Pagination
@@ -48,31 +49,31 @@ const SliderParallax = ({ruta}) => {
       <Swiper style={{ '--swiper-navigation-color': '#fff', '--swiper-pagination-color': '#fff' }} speed={600} parallax={true} pagination={{
         "clickable": true
       }} navigation={true} className="mySwiper">
-        <div slot="container-start" className="parallax-bg" style={{ 'background-image': `https://image.tmdb.org/t/p/w370_and_h556_bestv2/yyWNPhP1HR4BTLErHcZwIUsMBvA.jpg` }} data-swiper-parallax="-23%"> </div> 
         
         {
             resultados.map((resultado) => {
                 return (
                   <React.Fragment key={resultado.id}> 
                                
-                    <SwiperSlide>
-                      {console.log(`https://image.tmdb.org/t/p/w370_and_h556_bestv2${resultado.backdrop_path}`)}
-                    <div slot="container-start" className="parallax-bg" style={{ 'background-image': `https://image.tmdb.org/t/p/w370_and_h556_bestv2${resultado.backdrop_path}` }} data-swiper-parallax="-23%"> </div> 
-                      <div className="title" data-swiper-parallax="-300">
-                          { resultado.title
-                            ? resultado.title.length < 28 
-                                  ? resultado.title 
-                                  : recortarTitulo(resultado.title)
-                            : resultado.name 
-                              ? resultado.name.length < 28 
-                                ? resultado.name
-                                : recortarTitulo(resultado.name)   
-                              : resultado.name
-                         }
-                      </div>
-                      <div className="text" data-swiper-parallax="-100">
-                        <p>{resultado.overview}</p>
-                      </div>
+                    <SwiperSlide className="swiper-slide">
+                      <div slot="container-start" className="parallax-bg" style={{'background-image': `url(https://image.tmdb.org/t/p/w370_and_h556_bestv2/${resultado.backdrop_path})`}} data-swiper-parallax="-23%"></div>
+                   
+                        <div className="title" data-swiper-parallax="-300">
+                            { resultado.title
+                              ? resultado.title.length < 28 
+                                    ? resultado.title 
+                                    : recortarTitulo(resultado.title)
+                              : resultado.name 
+                                ? resultado.name.length < 28 
+                                  ? resultado.name
+                                  : recortarTitulo(resultado.name)   
+                                : resultado.name
+                          }
+                        </div>
+                        <div className="text" data-swiper-parallax="-100">
+                          <p>{resultado.overview}</p>
+                        </div>
+                    
                   </SwiperSlide>
                  
                   </React.Fragment>
