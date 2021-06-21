5d33a7a7225d645dc0517df70613f25fb79969eb
Lauriacunia <lauriacunia@gmail.com>
2021-06-21 11:56:28 -0300

    Arreglo el problema de abrir cardDetail desde el carousel de home

--- src/components/SliderParallax.js
+++ src/components/SliderParallax.js
@@ -1,27 +1,41 @@
 
 import React, { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { makeStyles } from '@material-ui/core/styles';
 import "swiper/swiper.min.css";
 import "swiper/components/pagination/pagination.min.css"
 import "swiper/components/navigation/navigation.min.css"
 import "./SliderParallax.css";
-import Boton from "./Boton";
-import {
-  useHistory
-  } 
-  from 'react-router-dom'
+import Button from '@material-ui/core/Button';
+import { useHistory } from 'react-router-dom'
+import cyan from '@material-ui/core/colors/cyan';
 
 
 import SwiperCore, {
-  Parallax, Navigation, Pagination
+  Parallax, Navigation, Pagination, Autoplay
 } from 'swiper/core';
 
-SwiperCore.use([Parallax, Navigation, Pagination]);
+SwiperCore.use([Parallax, Navigation, Pagination, Autoplay]);
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  boton: {
+      color:"white",
+      borderRadius: 0,
+  }
+}));
+
 
 
 const SliderParallax = ({ruta}) => {
   console.log(ruta)
   const history = useHistory();
+  const classes = useStyles();
 
   const [resultados, setResultados] = useState([]);
  
@@ -31,10 +45,6 @@ const SliderParallax = ({ruta}) => {
     return strCorto;
   };
 
-    const handleClick = (e) => {
-        console.log("hola")
-        console.log(e.target)
-    }
 
     useEffect(() => {
       console.log("entre al fecth de parallax")
@@ -53,9 +63,16 @@ const SliderParallax = ({ruta}) => {
   return (
     <>
 
-      <Swiper style={{ '--swiper-navigation-color': '#fff', '--swiper-pagination-color': '#fff' }} speed={600} parallax={true} pagination={{
-        "clickable": true
-      }} navigation={true} className="mySwiper">
+      <Swiper style={{ '--swiper-navigation-color': '#fff', '--swiper-pagination-color': '#fff' }} 
+              speed={600} 
+              parallax={true} pagination={{
+              "clickable": true
+              }} navigation={true} 
+              autoplay={{
+              "delay": 4000, 
+              "disableOnInteraction": false
+              }}
+              className="mySwiper">
         
         {
             resultados.map((resultado) => {
@@ -65,22 +82,32 @@ const SliderParallax = ({ruta}) => {
                     <SwiperSlide className="swiper-slide">
                       <div slot="container-start" className="parallax-bg" style={{'background-image': `url(https://image.tmdb.org/t/p/w370_and_h556_bestv2/${resultado.backdrop_path})`}} data-swiper-parallax="-23%"></div>
                    
-                        <div className="title" data-swiper-parallax="-300">
-                            { resultado.title
-                              ? resultado.title.length < 28 
-                                    ? resultado.title 
-                                    : recortarTitulo(resultado.title)
-                              : resultado.name 
-                                ? resultado.name.length < 28 
-                                  ? resultado.name
-                                  : recortarTitulo(resultado.name)   
-                                : resultado.name
-                          }
+                        <div>
+                          <div className="title" data-swiper-parallax="-300">
+                              { resultado.title
+                                ? resultado.title.length < 28 
+                                      ? resultado.title 
+                                      : recortarTitulo(resultado.title)
+                                : resultado.name 
+                                  ? resultado.name.length < 28 
+                                    ? resultado.name
+                                    : recortarTitulo(resultado.name)   
+                                  : resultado.name
+                            }
+                          </div>
+                          <div className="text" data-swiper-parallax="-100">
+                            <p>{resultado.overview}</p>
+                          </div> 
                         </div>
-                        <div className="text" data-swiper-parallax="-100">
-                          <p>{resultado.overview}</p>
-                        </div>
-                        <Boton onClick = {()=> { history.push("/movie")}}/>
+                    
+                        <Button className={classes.boton} color="primary" variant="contained"
+                                onClick = {()=> { console.log("hiciste click en la card ")
+                                                  history.push(`/movie/${resultado.id}`)}}>
+                                ver
+                        </Button>
+                        
+                        
+                 
                   </SwiperSlide>
                  
                   </React.Fragment>
